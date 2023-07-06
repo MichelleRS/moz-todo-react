329e007e0a78103a9fb231e07ae609defed0b8f4
Michelle Ross <miross629@gmail.com>
2023-07-06 10:40:56 -0700

    Build Form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Form from "./components/Form.js";
 import Todo from "./components/Todo.js";
 
 function App(props) {
@@ -14,43 +15,34 @@ function App(props) {
     <main>
       <h1>Todo App</h1>
       {/* form for user task input */}
-      <form>
-        <label htmlFor="new-todo-input">What needs to be done?</label>
-        <input
-          type="text"
-          id="new-todo-input"
-          name="text"
-          autoComplete="off"
-        ></input>
-        <button type="submit">Add</button>
-        {/* filter buttons */}
-        <div>
-          {/* all tasks */}
-          <button type="button" aria-pressed="true">
-            <span className="visually-hidden">show</span>
-            <span>All</span>
-            <span className="visually-hidden">tasks</span>
-          </button>
-          {/* active tasks */}
-          <button type="button" aria-pressed="false">
-            <span className="visually-hidden">show</span>
-            <span>Active</span>
-            <span className="visually-hidden">tasks</span>
-          </button>
-          {/* completed tasks */}
-          <button type="button" aria-pressed="false">
-            <span className="visually-hidden">show</span>
-            <span>Completed</span>
-            <span className="visually-hidden">tasks</span>
-          </button>
-        </div>
-        {/* TODO total tasks */}
-        <h2>Tasks to Complete</h2>
-        {/* list of tasks: list item with checkbox and update buttons */}
-        <ul role="list" aria-labelledby="list-heading">
-          {taskList}
-        </ul>
-      </form>
+      <Form />
+      {/* filter buttons */}
+      <div>
+        {/* all tasks */}
+        <button type="button" aria-pressed="true">
+          <span className="visually-hidden">show</span>
+          <span>All</span>
+          <span className="visually-hidden">tasks</span>
+        </button>
+        {/* active tasks */}
+        <button type="button" aria-pressed="false">
+          <span className="visually-hidden">show</span>
+          <span>Active</span>
+          <span className="visually-hidden">tasks</span>
+        </button>
+        {/* completed tasks */}
+        <button type="button" aria-pressed="false">
+          <span className="visually-hidden">show</span>
+          <span>Completed</span>
+          <span className="visually-hidden">tasks</span>
+        </button>
+      </div>
+      {/* TODO total tasks */}
+      <h2>Tasks to Complete</h2>
+      {/* list of tasks: list item with checkbox and update buttons */}
+      <ul role="list" aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </main>
   );
 }
